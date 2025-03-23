5ef1a6484116c7cb4b09777a87533cf01e26bb1f
Pierre-Emmanuel <pierre.emmanuel41@gmail.com>
2025-03-23 15:10:26 +0100

    Updating docummentation

--- src/main/java/fr/pederobien/communication/interfaces/client/IClient.java
+++ src/main/java/fr/pederobien/communication/interfaces/client/IClient.java
@@ -9,25 +9,17 @@ public interface IClient {
 	 * method is called. The class is expected to retry establishing the connection
 	 * as long as Disconnected() is not called. Timeout may be reported in event
 	 * LogEvent.
-	 *
-	 * @return true if the client is in correct state to start the connection to the
-	 *         remote.
 	 */
 	void connect();
 
 	/**
 	 * Close the connection to the remote.
-	 *
-	 * @return true if the client is in correct state to close the connection, false
-	 *         otherwise.
 	 */
 	void disconnect();
 
 	/**
 	 * Dispose this connection. After this, it is impossible to send data to the
 	 * remote using this connection.
-	 *
-	 * @return true if the client has been disposed, false otherwise.
 	 */
 	void dispose();
 
