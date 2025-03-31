7007b6e2daa30f3fa41e294b545c714e71d7aa32
Pierre-Emmanuel <pierre.emmanuel41@gmail.com>
2025-03-31 20:01:25 +0200

    Fix: compilation errors

--- src/main/java/fr/pederobien/communication/testing/tools/ClientExceptionImpl.java
+++ src/main/java/fr/pederobien/communication/testing/tools/ClientExceptionImpl.java
@@ -1,10 +1,7 @@
 package fr.pederobien.communication.testing.tools;
 
-import fr.pederobien.communication.impl.Communication;
 import fr.pederobien.communication.interfaces.IEthernetEndPoint;
-import fr.pederobien.communication.interfaces.client.IClientConfig;
 import fr.pederobien.communication.interfaces.client.IClientImpl;
-import fr.pederobien.communication.interfaces.connection.IConnection;
 import fr.pederobien.communication.interfaces.connection.IConnectionImpl;
 
 public class ClientExceptionImpl implements IClientImpl<IEthernetEndPoint> {
@@ -26,8 +23,8 @@ public class ClientExceptionImpl implements IClientImpl<IEthernetEndPoint> {
 	}
 
 	@Override
-	public IConnection connect(IClientConfig<IEthernetEndPoint> config) throws Exception {
-		return Communication.createConnection(config, config.getEndPoint(), impl);
+	public IConnectionImpl connect(String name, IEthernetEndPoint endPoint, int timeout) throws Exception {
+		return impl;
 	}
 
 	private class ConnectionExceptionImpl implements IConnectionImpl {
